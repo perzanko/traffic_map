74195a670f8991f9f23959721a148bf3261aa05f
Kacper Perzankowski <perzankowski.kacper@gmail.com>
2019-11-23 03:35:44 +0100

    init

---
+++ assets/js/src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import "phoenix_html"
+import {Socket} from "phoenix"
+
+import App from './App';
+
+window.observationsSocket = new Socket("/socket", {params: {}})
+
+window.observationsSocket.connect()
+
+const channel = window.observationsSocket.channel('observations', {});
+
+channel.connect()
+  .receive("ok", resp => { console.log("ok", resp) })
+  .receive("error", resp => { console.log("error", resp) })
+
+ReactDOM.render(<App />, document.getElementById('root'));
